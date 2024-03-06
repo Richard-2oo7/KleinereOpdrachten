17d3b4cd1e5f18b4bbf9e0ad6b32b8d080d3a1b5
Richard <rsdevries07@gmail.com>
2024-03-06 09:31:07 +0100

    verbeter

--- voeg mensen toe/javascript.js
+++ voeg mensen toe/javascript.js
@@ -7,13 +7,9 @@ knop.onclick = function (){
    plaatje.src = "imgpersoon.jpg";
    plaatje.id = "profile";
    mensendiv.appendChild(plaatje);
-    var appel = {
-         hoi : 2,
-         sap : sap
-    }
    var naam = input.value;
-   plaatje.appel = naam;
+   plaatje.addvarisnotafunction = naam;
    plaatje.onclick = function (){
-        console.log(plaatje.appel);
+        console.log(plaatje.addvarisnotafunction);
    }   
 }
